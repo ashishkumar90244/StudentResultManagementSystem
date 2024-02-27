d8a881414336b674684c1d001ea2a28cbdd6d4d8
ashishkumar90244 <ashish.kumarcs0101@gmail.com>
2024-02-27 22:55:58 +0530

    first commit

---
+++ src/student/result/management/StudentResultManagement.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.result.management;
+
+
+
+
+/**
+ *
+ * @author hp
+ */
+public class StudentResultManagement {
+
+    public StudentResultManagement(){
+        
+        
+        
+       // new index().setVisible(true);
+    }
+
+    
+    
+    
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new StudentResultManagement();
+        // TODO code application logic here
+    }
+    
+}
